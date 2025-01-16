56e8206b6dddc1e97b8260a681c4628635dc31ce
hendisantika <hendisantika@yahoo.co.id>
2025-01-17 05:43:00 +0700

    Add ErrorHandler🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/handler/ErrorHandler.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.vertx_reactive_rest_api.api.handler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-reactive-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/25
+ * Time: 05.41
+ * To change this template use File | Settings | File Templates.
+ */
+public class ErrorHandler {
+  private ErrorHandler() {
+  }
+}
