7dfd975907974b41d519e1efdc7145015af9098a
hendisantika <hendisantika@yahoo.co.id>
2025-01-17 05:43:13 +0700

    Add buildHandler in ErrorHandler🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/handler/ErrorHandler.java
+++ src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/handler/ErrorHandler.java
@@ -1,5 +1,12 @@
 package id.my.hendisantika.vertx_reactive_rest_api.api.handler;
 
+import id.my.hendisantika.vertx_reactive_rest_api.api.utils.ResponseUtils;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.validation.BadRequestException;
+import io.vertx.ext.web.validation.BodyProcessorException;
+import io.vertx.ext.web.validation.ParameterProcessorException;
+import io.vertx.ext.web.validation.RequestPredicateException;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : vertx-reactive-rest-api
@@ -12,5 +19,25 @@ package id.my.hendisantika.vertx_reactive_rest_api.api.handler;
  */
 public class ErrorHandler {
   private ErrorHandler() {
+  }
+
+  /**
+   * Build error handler
+   * It's useful to handle errors thrown by Web Validation
+   *
+   * @param router Router
+   */
+  public static void buildHandler(Router router) {
+    router.errorHandler(400, rc -> {
+      if (rc.failure() instanceof BadRequestException) {
+        if (rc.failure() instanceof ParameterProcessorException) {
+          // Something went wrong while parsing/validating a parameter
+          ResponseUtils.buildErrorResponse(rc, new IllegalArgumentException("Path parameter is invalid"));
+        } else if (rc.failure() instanceof BodyProcessorException | rc.failure() instanceof RequestPredicateException) {
+          // Something went wrong while parsing/validating the body
+          ResponseUtils.buildErrorResponse(rc, new IllegalArgumentException("Request body is invalid"));
+        }
+      }
+    });
   }
 }
