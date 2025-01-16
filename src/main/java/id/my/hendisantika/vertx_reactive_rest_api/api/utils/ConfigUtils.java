757c261bf918b8b811b6ec4ef22f045f3730cb64
hendisantika <hendisantika@yahoo.co.id>
2025-01-17 05:32:15 +0700

    Add ConfigUtils🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/utils/ConfigUtils.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.vertx_reactive_rest_api.api.utils;
+
+import java.util.Properties;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-reactive-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/25
+ * Time: 05.32
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfigUtils {
+  private static ConfigUtils instance;
+
+  private final Properties properties;
+
+}
