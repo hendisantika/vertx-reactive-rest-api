6b6f63c47d4861e7ad301ccaa2c1004cf4a54188
hendisantika <hendisantika@yahoo.co.id>
2025-01-17 05:51:43 +0700

    Add buildHttpServer in ApiVerticle🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_reactive_rest_api/verticle/ApiVerticle.java
+++ src/main/java/id/my/hendisantika/vertx_reactive_rest_api/verticle/ApiVerticle.java
@@ -9,8 +9,10 @@ import id.my.hendisantika.vertx_reactive_rest_api.api.router.HealthCheckRouter;
 import id.my.hendisantika.vertx_reactive_rest_api.api.router.MetricsRouter;
 import id.my.hendisantika.vertx_reactive_rest_api.api.service.BookService;
 import id.my.hendisantika.vertx_reactive_rest_api.utils.DbUtils;
+import id.my.hendisantika.vertx_reactive_rest_api.utils.LogUtils;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
@@ -48,4 +50,30 @@ public class ApiVerticle extends AbstractVerticle {
 
     buildHttpServer(vertx, promise, router);
   }
+
+  /**
+   * Run HTTP server on port 8888 with specified routes
+   *
+   * @param vertx   Vertx context
+   * @param promise Callback
+   * @param router  Router
+   */
+  private void buildHttpServer(Vertx vertx,
+                               Promise<Void> promise,
+                               Router router) {
+    final int port = 8888;
+
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(port, http -> {
+        if (http.succeeded()) {
+          promise.complete();
+          LOGGER.info(LogUtils.RUN_HTTP_SERVER_SUCCESS_MESSAGE.buildMessage(port));
+        } else {
+          promise.fail(http.cause());
+          LOGGER.info(LogUtils.RUN_HTTP_SERVER_ERROR_MESSAGE.buildMessage());
+        }
+      });
+  }
+
 }
