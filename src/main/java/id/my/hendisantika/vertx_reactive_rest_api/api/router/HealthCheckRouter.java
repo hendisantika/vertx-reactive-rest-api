776b97bda49af9a207d5d0972939c20f64053fa9
hendisantika <hendisantika@yahoo.co.id>
2025-01-17 05:46:49 +0700

    Add setRouter in HealthCheckRouter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/router/HealthCheckRouter.java
+++ src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/router/HealthCheckRouter.java
@@ -1,5 +1,11 @@
 package id.my.hendisantika.vertx_reactive_rest_api.api.router;
 
+import io.vertx.core.Vertx;
+import io.vertx.ext.healthchecks.HealthCheckHandler;
+import io.vertx.ext.healthchecks.Status;
+import io.vertx.ext.web.Router;
+import io.vertx.pgclient.PgPool;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : vertx-reactive-rest-api
@@ -12,5 +18,32 @@ package id.my.hendisantika.vertx_reactive_rest_api.api.router;
  */
 public class HealthCheckRouter {
   private HealthCheckRouter() {
+  }
+
+  /**
+   * Set health check routes
+   *
+   * @param vertx    Vertx context
+   * @param router   Router
+   * @param dbClient PostgreSQL pool
+   */
+  public static void setRouter(Vertx vertx,
+                               Router router,
+                               PgPool dbClient) {
+    final HealthCheckHandler healthCheckHandler = HealthCheckHandler.create(vertx);
+
+    healthCheckHandler.register("database",
+      promise ->
+        dbClient.getConnection(connection -> {
+          if (connection.failed()) {
+            promise.fail(connection.cause());
+          } else {
+            connection.result().close();
+            promise.complete(Status.OK());
+          }
+        })
+    );
+
+    router.get("/health").handler(healthCheckHandler);
   }
 }
