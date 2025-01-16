96e6ae21ed30fbe31c9669514a84e189e08880d4
hendisantika <hendisantika@yahoo.co.id>
2025-01-17 05:29:54 +0700

    Add selectAll in BookRepository🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/repository/BookRepository.java
+++ src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/repository/BookRepository.java
@@ -1,7 +1,15 @@
 package id.my.hendisantika.vertx_reactive_rest_api.api.repository;
 
+import id.my.hendisantika.vertx_reactive_rest_api.api.model.Book;
+import io.vertx.core.Future;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.sqlclient.SqlConnection;
+import io.vertx.sqlclient.templates.SqlTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +34,30 @@ public class BookRepository {
   private static final String SQL_COUNT = "SELECT COUNT(*) AS total FROM books";
 
   public BookRepository() {
+  }
+
+  /**
+   * Read all books using pagination
+   *
+   * @param connection PostgreSQL connection
+   * @param limit      Limit
+   * @param offset     Offset
+   * @return List<Book>
+   */
+  public Future<List<Book>> selectAll(SqlConnection connection,
+                                      int limit,
+                                      int offset) {
+    return SqlTemplate
+      .forQuery(connection, SQL_SELECT_ALL)
+      .mapTo(Book.class)
+      .execute(Map.of("limit", limit, "offset", offset))
+      .map(rowSet -> {
+        final List<Book> books = new ArrayList<>();
+        rowSet.forEach(books::add);
+
+        return books;
+      })
+      .onSuccess(success -> LOGGER.info(LogUtils.REGULAR_CALL_SUCCESS_MESSAGE.buildMessage("Read all books", SQL_SELECT_ALL)))
+      .onFailure(throwable -> LOGGER.error(LogUtils.REGULAR_CALL_ERROR_MESSAGE.buildMessage("Read all books", throwable.getMessage())));
   }
 }
