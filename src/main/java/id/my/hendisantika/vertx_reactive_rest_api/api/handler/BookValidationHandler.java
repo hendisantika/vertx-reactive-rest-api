7b3006837432e49b05c7a530d77d1d29e1008bda
hendisantika <hendisantika@yahoo.co.id>
2025-01-17 05:43:34 +0700

    Add BookValidationHandler🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/vertx_reactive_rest_api/api/handler/BookValidationHandler.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.vertx_reactive_rest_api.api.handler;
+
+import io.vertx.core.Vertx;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : vertx-reactive-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/01/25
+ * Time: 05.43
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookValidationHandler {
+  private final Vertx vertx;
+
+  public BookValidationHandler(Vertx vertx) {
+    this.vertx = vertx;
+  }
+}
